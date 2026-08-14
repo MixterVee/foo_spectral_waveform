4c8fa2e4f2a2a864720bf52bc99713b0974224fb
MixterVee <mixtervandal@gmail.com>
2026-08-14 15:30:57 -0400

    Bump spectral waveform to v0.3 alpha

--- foo_spectral_waveform.cpp
+++ foo_spectral_waveform.cpp
@@ -4,10 +4,10 @@
 
 DECLARE_COMPONENT_VERSION(
     "Spectral Waveform",
-    "0.2.0-alpha",
+    "0.3.0-alpha",
     "Serato-inspired spectral waveform seekbar for foobar2000.\n"
     "\n"
-    "v0.2 development build: Default UI element, playback cursor and click-to-seek."
+    "v0.3 development build: decoded spectral waveform, click-to-seek, mouse-wheel zoom and drag panning."
 );
 
 VALIDATE_COMPONENT_FILENAME("foo_spectral_waveform.dll");
@@ -17,7 +17,7 @@ namespace {
 class spectral_initquit : public initquit {
 public:
     void on_init() override {
-        console::print("foo_spectral_waveform: v0.2.0-alpha loaded");
+        console::print("foo_spectral_waveform: v0.3.0-alpha loaded");
     }
 
     void on_quit() override {}
