cd4df244ba250d31a215ed8c71115f128cfb048b
MixterVee <mixtervandal@gmail.com>
2026-08-14 15:05:10 -0400

    Store RMS envelope data for waveform scaling

--- waveform_data.h
+++ waveform_data.h
@@ -6,6 +6,7 @@ namespace spectral_waveform {
 
 struct waveform_point {
     std::uint16_t peak = 0;
+    std::uint16_t rms = 0;
     std::uint8_t bass = 0;
     std::uint8_t mids = 0;
     std::uint8_t treble = 0;
@@ -15,6 +16,7 @@ struct waveform_data {
     double duration_seconds = 0.0;
     std::uint32_t sample_rate = 0;
     std::uint32_t channels = 0;
+    std::uint16_t rms_reference = 0;
     std::vector<waveform_point> points;
 };
 
