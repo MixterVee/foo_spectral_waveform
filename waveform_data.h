56a32a026dee5e7b260e1990cb0f1b92b8c4b827
MixterVee <mixtervandal@gmail.com>
2026-08-14 13:33:47 -0400

    Add waveform data structures

---
+++ waveform_data.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <cstdint>
+#include <vector>
+
+namespace spectral_waveform {
+
+struct waveform_point {
+    std::uint16_t peak = 0;
+    std::uint8_t bass = 0;
+    std::uint8_t mids = 0;
+    std::uint8_t treble = 0;
+};
+
+struct waveform_data {
+    double duration_seconds = 0.0;
+    std::uint32_t sample_rate = 0;
+    std::uint32_t channels = 0;
+    std::vector<waveform_point> points;
+};
+
+} // namespace spectral_waveform
