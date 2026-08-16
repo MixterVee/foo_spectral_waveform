10b119429bb73d11b625022a3c639ab29745a22a
MixterVee <mixtervandal@gmail.com>
2026-08-16 01:32:40 -0400

    Add shared stem transport interface

---
+++ stem_transport_service.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <foobar2000/SDK/foobar2000.h>
+
+enum stem_transport_state : int {
+    stem_transport_normal = 0,
+    stem_transport_hold = 1,
+    stem_transport_scrub = 2,
+    stem_transport_reverse = 3,
+    stem_transport_release_wait = 4,
+};
+
+class NOVTABLE stem_transport_service : public service_base {
+    FB2K_MAKE_SERVICE_INTERFACE_ENTRYPOINT(stem_transport_service);
+public:
+    virtual void set_hold(double position_seconds) = 0;
+    virtual void set_scrub(double position_seconds) = 0;
+    virtual void set_reverse(double position_seconds) = 0;
+    virtual void release_transport(double position_seconds) = 0;
+    virtual void cancel_transport() = 0;
+
+    virtual int get_state() = 0;
+    virtual double get_position_seconds() = 0;
+    virtual bool is_position_ready(double position_seconds) = 0;
+};
