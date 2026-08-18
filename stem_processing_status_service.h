00395d182c217a446640d5f6c4f57f1f0f2f90b3
MixterVee <mixtervandal@gmail.com>
2026-08-18 09:52:17 -0400

    Add shared stem processing status interface

---
+++ stem_processing_status_service.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <foobar2000/SDK/foobar2000.h>
+
+enum stem_processing_backend_type : int {
+    stem_processing_backend_unknown = -1,
+    stem_processing_backend_cpu = 0,
+    stem_processing_backend_directml = 1,
+};
+
+struct stem_processing_status {
+    int mode = 0;
+    int backend_type = stem_processing_backend_unknown;
+    unsigned adapter_index = 0;
+    int engine_ready = 0;
+    int processing = 0;
+    int cpu_fallback = 0;
+    wchar_t backend_label[128]{};
+};
+
+class NOVTABLE stem_processing_status_service : public service_base {
+    FB2K_MAKE_SERVICE_INTERFACE_ENTRYPOINT(stem_processing_status_service);
+public:
+    virtual bool get_status(stem_processing_status& out) = 0;
+};
